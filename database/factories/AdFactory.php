30391d77ae0b72cc1f8e5a3ba1fac7b892d576ef
ChamkhiAnas <mahmoudbennouna@gmail.com>
2020-09-08 15:39:04 +0100

    model

---
+++ database/factories/AdFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Ad;
+use Faker\Generator as Faker;
+
+$factory->define(Ad::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
